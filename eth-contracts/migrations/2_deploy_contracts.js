106f1f49fae7650a0053e8d2da2ef6c045e0df72
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-05-28 13:08:30 +0530

    remove unused code

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
 // migrating the appropriate contracts
 var Verifier = artifacts.require("./Verifier.sol");
-// var ERC721MintableComplete = artifacts.require("./ERC721MintableComplete.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Verifier);
-  // await deployer.deploy(ERC721MintableComplete);
   await deployer.deploy(
     SolnSquareVerifier,
     Verifier.address,
