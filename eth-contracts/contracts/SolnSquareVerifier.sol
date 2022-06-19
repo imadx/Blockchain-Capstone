d6a43a7da7a25ec2e85ccf769af8bfb217333263
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-19 16:59:08 +0530

    update tests on solnSquareVerifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,20 +1,12 @@
 pragma solidity >=0.4.21 <0.6.0;
+pragma experimental ABIEncoderV2;
 
 import "./ERC721MintableComplete.sol";
+import "./Verifier.sol";
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract SquareVerifier {
-    function verifyTx(
-        uint256[2] memory A,
-        uint256[2] memory A_p,
-        uint256[2][2] memory B,
-        uint256[2] memory B_p,
-        uint256[2] memory C,
-        uint256[2] memory C_p,
-        uint256[2] memory H,
-        uint256[2] memory K,
-        uint256[2] memory input
-    ) public returns (bool);
+  function verifyTx(Verifier.Proof memory proof, uint256[2] memory input) public returns (bool);
 }
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
@@ -34,84 +26,53 @@ contract SquareVerifier {
 //  - make sure you handle metadata as well as tokenSuplly
 
 contract SolnSquareVerifier is ERC721MintableComplete {
-    SquareVerifier private verifierContract;
+  SquareVerifier private verifierContract;
 
-    struct Solution {
-        uint256 solutionIndex;
-        address solutionAddress;
-    }
+  struct Solution {
+    uint256 solutionIndex;
+    address solutionAddress;
+  }
 
-    uint256 nextSolutionIndex = 0;
-    Solution[] private _solutions;
+  uint256 nextSolutionIndex = 0;
+  Solution[] private _solutions;
 
-    mapping(bytes32 => Solution) solutions;
-    mapping(bytes32 => bool) isSolutionMinted;
+  mapping(bytes32 => Solution) solutions;
+  mapping(bytes32 => bool) isSolutionMinted;
 
-    event SolutionSubmitted(uint256 solutionIndex, address solutionAddress);
+  event SolutionSubmitted(uint256 solutionIndex, address solutionAddress);
 
-    constructor(
-        address verifierAddress,
-        string memory name,
-        string memory symbol
-    ) public ERC721MintableComplete(name, symbol) {
-        verifierContract = SquareVerifier(verifierAddress);
-    }
+  constructor(
+    address verifierAddress,
+    string memory name,
+    string memory symbol
+  ) public ERC721MintableComplete(name, symbol) {
+    verifierContract = SquareVerifier(verifierAddress);
+  }
 
-    function addSolution(
-        uint256[2] memory A,
-        uint256[2] memory A_p,
-        uint256[2][2] memory B,
-        uint256[2] memory B_p,
-        uint256[2] memory C,
-        uint256[2] memory C_p,
-        uint256[2] memory H,
-        uint256[2] memory K,
-        uint256[2] memory input
-    ) public {
-        bytes32 solutionHash = keccak256(abi.encodePacked(input[0], input[1]));
-        require(
-            solutions[solutionHash].solutionAddress == address(0),
-            "Solution already exists"
-        );
+  function addSolution(Verifier.Proof memory proof, uint256[2] memory input) public {
+    bytes32 solutionHash = keccak256(abi.encodePacked(input[0], input[1]));
+    require(solutions[solutionHash].solutionAddress == address(0), "Solution already exists");
 
-        bool verified = verifierContract.verifyTx(
-            A,
-            A_p,
-            B,
-            B_p,
-            C,
-            C_p,
-            H,
-            K,
-            input
-        );
-        require(verified, "Solution could not be verified");
+    bool verified = verifierContract.verifyTx(proof, input);
+    require(verified, "Solution could not be verified");
 
-        solutions[solutionHash] = Solution(nextSolutionIndex, msg.sender);
-        emit SolutionSubmitted(nextSolutionIndex, msg.sender);
-        nextSolutionIndex++;
-    }
+    solutions[solutionHash] = Solution(nextSolutionIndex, msg.sender);
+    emit SolutionSubmitted(nextSolutionIndex, msg.sender);
+    nextSolutionIndex++;
+  }
 
-    function mint(
-        uint256 a,
-        uint256 b,
-        address to
-    ) public {
-        bytes32 solutionHash = keccak256(abi.encodePacked(a, b));
-        require(
-            solutions[solutionHash].solutionAddress != address(0),
-            "Non-existent solution"
-        );
-        require(
-            isSolutionMinted[solutionHash] == false,
-            "Already minted solution"
-        );
-        require(
-            solutions[solutionHash].solutionAddress == msg.sender,
-            "msg.sender is not the solution owner"
-        );
+  function mintToken(
+    uint256 a,
+    uint256 b,
+    uint256 tokenId,
+    address to
+  ) public {
+    bytes32 solutionHash = keccak256(abi.encodePacked(a, b));
+    require(solutions[solutionHash].solutionAddress != address(0), "Non-existent solution");
+    require(isSolutionMinted[solutionHash] == false, "Already minted solution");
+    require(solutions[solutionHash].solutionAddress == msg.sender, "msg.sender is not the solution owner");
 
-        super.mint(to, solutions[solutionHash].solutionIndex);
-        isSolutionMinted[solutionHash] = true;
-    }
+    super.mint(to, tokenId);
+    isSolutionMinted[solutionHash] = true;
+  }
 }
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,34 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
-
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+const Verifier = artifacts.require("Verifier");
+const Proof = require("../../zokrates/code/square/proof");
+
+const truffleAssert = require("truffle-assertions");
+
+contract("TestSolnSquareVerifier", (accounts) => {
+  let contract;
+
+  before(async function () {
+    let verifier = await Verifier.new({ from: accounts[0] });
+    contract = await SolnSquareVerifier.new(verifier.address, "Test Token", "TTKN01", { from: accounts[0] });
+  });
+
+  it("should add a new solution", async () => {
+    await contract.addSolution(Proof.proof, Proof.inputs);
+  });
+
+  it("should mint a new token", async () => {
+    await contract.mintToken(Proof.inputs[0], Proof.inputs[1], 101, accounts[1], { from: accounts[0] });
+    const ownerAddress = await contract.ownerOf.call(101, { from: accounts[0] });
+    expect(ownerAddress).to.equal(accounts[1]);
+  });
+
+  it("should fail for existing solutions", async () => {
+    await truffleAssert.reverts(
+      contract.mintToken(Proof.inputs[0], Proof.inputs[1], 101, accounts[1], { from: accounts[0] }),
+      "Already minted solution"
+    );
+  });
+});
